73ea24a2cb9cc26b0a7e4d14adaff965fd2f1f1c
ODAJIM17 <Davila56jd@gmail.com>
2025-08-21 11:39:43 -0400

    Added Country entity

---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities;
+
+public class Country
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Display(Name = "Country")]
+    [MaxLength(100, ErrorMessage = "{0} can not exceed {1} characters.")]
+    [Required(ErrorMessage = "{0} is required.")]
+    public string Name { get; set; } = null!;
+}
