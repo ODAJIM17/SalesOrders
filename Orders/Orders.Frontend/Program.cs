8e082324f689157727259313174ea96cce068dad
ODAJIM17 <Davila56jd@gmail.com>
2025-09-19 15:33:56 -0400

    Add Countries component

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -1,5 +1,6 @@
 using MudBlazor.Services;
 using Orders.Frontend.Components;
+using Orders.Frontend.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddMudServices();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7177") });
+builder.Services.AddScoped<IRepository, Repository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,7 +24,6 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 
 app.UseAntiforgery();
 
