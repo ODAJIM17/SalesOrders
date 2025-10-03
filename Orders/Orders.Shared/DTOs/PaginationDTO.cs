d76881dedd6b445402ec276fba076c3d05a75b28
ODAJIM17 <Davila56jd@gmail.com>
2025-10-03 14:43:38 -0400

    Country Index Filter

--- Orders/Orders.Shared/DTOs/PaginationDTO.cs
+++ Orders/Orders.Shared/DTOs/PaginationDTO.cs
@@ -13,5 +13,6 @@ namespace Orders.Shared.DTOs
         public int Page { get; set; } = 1;
 
         public int RecordsNumber { get; set; } = 10;
+        public string? Filter { get; set; }
     }
 }
